7b16cf2d0b5d61b61a6fa76c318cf8e428e29fe8
Matheus Silva Freitas <msformigteen@live.com>
2023-05-07 18:43:55 -0300

    feat: start

---
+++ mortal.pl
@@ -0,0 +1,14 @@
+% MORTAL - Programa em Prolog para definir os Mortais
+
+mortal(X) :- person(X).
+
+person(socrates).
+person(plato).
+person(zeno).
+person(aristotle).
+
+mortal_report:-
+    write('Known mortals are:'),nl,
+    mortal(X),
+    write(X),nl,
+fail.
